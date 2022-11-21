852312634ed147c8a135b6874c143db7a1d7f87d
Eladio Feijoo <eladio.feijoo@gmal.com>
2022-11-21 18:59:08 -0400

    feat(avatar): add Avatar component

---
+++ components/avatar/Avatar.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import profilePic from "../../public/me.png";
+
+import styles from "./styles.module.css";
+
+export const Avatar = () => {
+  return (
+    <Image
+      className={styles.profileImage}
+      src={profilePic}
+      alt="Picture of the author"
+    />
+  );
+};
