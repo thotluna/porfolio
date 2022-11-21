9dfcd9df6631465f1cdaee930ab4e9f8e9f6a103
Eladio Feijoo <eladio.feijoo@gmal.com>
2022-11-21 19:00:01 -0400

    feat(footer): add Footer component

---
+++ components/footer/Footer.tsx
@@ -0,0 +1,9 @@
+import styles from "./styles.module.css";
+
+export const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <p style={{ textAlign: "center" }}>Desarrollado por Eladio Feijóo</p>
+    </footer>
+  );
+};
