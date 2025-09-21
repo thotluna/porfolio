459c9704dd877b8c654badf1a22a812541fee123
Eladio Feijoo <eladio.feijoo@gmail.com>
2025-09-21 15:08:52 +0200

    add weangler lib

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,14 @@ import tailwindcss from '@tailwindcss/vite';
 
 import icon from 'astro-icon';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
 
-  integrations: [icon()]
+  integrations: [icon()],
+  adapter: cloudflare()
 });
